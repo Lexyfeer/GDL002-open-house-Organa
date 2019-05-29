71e39a2dfbdec8c9fad83be6c497d6d65cdc59ce
Hanhelle <hanhelle@gmail.com>
2019-05-29 17:19:59 -0500

    compoente fecha y componente QR

---
+++ src/components/Date.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import moment from 'moment';
+
+
+
+
+export default class Date extends React.Component {
+    render() {
+        return (
+            <h5>
+               
+                {moment().format('LLLL')}
+            </h5>
+        );
+    }
+}
+
+ 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Logo from '../imagenes/logoL.jpeg';
+import Date from './Date';
 
 const Navbar = () => (
     <nav>
       <img src={Logo} className="logoLink" alt="logo" />
       <ul className="navMenu">
-          <li><NavLink className="navMenuLink" to="./Students"> Alumna </NavLink></li>
+          <li><NavLink className="navMenuLink" to="./Students"> Estudiante </NavLink></li>
           <li><NavLink className="navMenuLink" to="./Admin"> Admin </NavLink></li>
       </ul>
       <div>
-        <h4> Fecha </h4>
+        <Date/>
       </div>
     </nav>
 
