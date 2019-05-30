3fd6736462a08972f0e8c39ea05965afe5c47405
Nancy <lexyfeer@hotmail.com>
2019-05-30 17:26:06 -0500

    Cambie firebase la forma de llamarlo

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,19 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/firebase-firestore';
 // Add additional services that you want to use
 // require("firebase/database");
 
 // Initialize Firebase
 // TODO: Replace with your project's customized code snippet
-var firebaseConfig = {
-  apiKey: "AIzaSyBVwMSxoh4jwZDa_GKoPpeLNTltVpIE3lA",
-  authDomain: "organa-1020.firebaseapp.com",
-  databaseURL: "https://organa-1020.firebaseio.com",
-  projectId: "organa-1020",
-  storageBucket: "organa-1020.appspot.com",
-  messagingSenderId: "893591829196",
-  appId: "1:893591829196:web:faa21b24f9907250"
-};
-firebase.initializeApp(firebaseConfig);
-export default firebase;
+firebase.initializeApp ({
+    apiKey: "AIzaSyAC3UgTIuxUjvcUA79P97hfmbB-VUTDTi0",
+    authDomain: "controlasistenciaorgana.firebaseapp.com",
+    databaseURL: "https://controlasistenciaorgana.firebaseio.com",
+    projectId: "controlasistenciaorgana",
+    storageBucket: "controlasistenciaorgana.appspot.com",
+    messagingSenderId: "1025261164858",
+    appId: "1:1025261164858:web:30cb65062cdca2de"
+});
+let db = firebase.firestore();
+db.settings({timestampsInSnapshots:true});
+export default db;
